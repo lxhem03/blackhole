7b3e4255d1433f87799a6431433f09d7bf1328d3
Levhcwm21. <alexags1983@gmail.com>
2025-10-21 21:11:27 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -12,6 +12,7 @@ import re
 import json
 import subprocess
 import math
+import shlex
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
