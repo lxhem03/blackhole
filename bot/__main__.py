a86df09c56ca94c123c24deb955dcc3168f3516c
Levhcwm21. <alexags1983@gmail.com>
2025-10-27 19:21:33 +0530

    Bugs fixed

--- bot/__main__.py
+++ bot/__main__.py
@@ -14,6 +14,7 @@ import os, asyncio, pyrogram, psutil, platform
 from bot import app
 from bot.config import Config
 from bot.plugins.call_back_button_handler import button
+from pyrogram.handlers import CallbackQueryHandler
 
 uptime = dt.now()
 
